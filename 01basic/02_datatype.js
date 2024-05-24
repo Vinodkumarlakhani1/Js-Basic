481e12a5e9292679cd9c643dbd96b18f2f26c821
Vinod kumar lakhani <10224016+Vinodkumarlakhani1@users.noreply.github.com>
2024-05-24 21:58:47 +0000

    datatype

---
+++ 01basic/02_datatype.js
@@ -0,0 +1,36 @@
+// "use strict" ; // treat all JS code as newer version
+// alert("hello world"); // we are using node js , not a browser alert only use on browser
+
+console.log(3 + 3);
+ console.log("vinod"); 
+
+
+
+let name = "vinod";
+let age = 18;
+// let isLoggedIn = flase;
+
+let state;  // undefined
+
+
+
+// number => 2 to power 53
+// bigint 
+// String => ""
+// Boolean  => true or false
+// null   =>     stand alone value ha  null kya h k representation value of empty
+// undefined  =>   value 
+// symbol =>  unique 
+
+
+
+
+// Object
+
+
+console.log(typeof "vinod"); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); //null data type object
+
+
+
