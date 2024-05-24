db7ae51734226cd0583299179d696fe40a421572
Vinod kumar lakhani <10224016+Vinodkumarlakhani1@users.noreply.github.com>
2024-05-24 20:38:37 +0000

    01 variable

---
+++ 01basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 12253; //not change the data 
+let accountEmail = "lakhani@gmail.com";
+let accountPassword = "12346";
+let accountCity = "Karachi";
+
+// accountId = 2; not allowed
+// console.log(accountId);
+
+accountEmail = "vinodkumar@gmail.com";
+accountPassword = "112555";
+accountCity = "lahore";
+
+console.table(accountId, accountEmail,accountPassword,accountCity);
+
+ 
