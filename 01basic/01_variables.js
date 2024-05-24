1c9630f9bdef27e930d25da70d3e70475140c1b5
Vinod kumar lakhani <10224016+Vinodkumarlakhani1@users.noreply.github.com>
2024-05-24 19:36:03 +0000

    variables and for loop

---
+++ 01basic/01_variables.js
@@ -0,0 +1,12 @@
+// const acountId = 123;
+var n = 5;
+for (let i = 1; i <= n; i++) {
+    let pt = "";
+    for (let j = 1; j <=i; j++) {
+        pt = pt+j+" ";
+        
+    }
+    console.log(pt);
+    
+    
+}
