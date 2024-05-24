af8bdacff45be89e445ddc99c753b14be3b47178
Vinod kumar lakhani <10224016+Vinodkumarlakhani1@users.noreply.github.com>
2024-05-24 22:56:16 +0000

    Comparison

---
+++ 01basic/04_comparison.js
@@ -0,0 +1,31 @@
+// comparison
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+/*
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);  // yahan pe value conversion aajati h
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0); 
+ass tarike k comparison kabhi bhi canfuze kr sakte hain
+
+*/
+// === strke comparison check the data type
+
+console.log("2" === 2);
+
+
+
+
+
