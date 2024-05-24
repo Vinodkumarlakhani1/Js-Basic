ce8c96eb46bfe3e9d05d84dd478bef567e50188d
Vinod kumar lakhani <10224016+Vinodkumarlakhani1@users.noreply.github.com>
2024-05-24 22:40:26 +0000

    operation

--- 01basic/03_datatypeconversion.js
+++ 01basic/03_datatypeconversion.js
@@ -36,6 +36,63 @@ let isLoggedIn = "vinod"; //agr hum string ko boolen mn convert kari n gi to ham
   console.log(typeof stringNumber);
   console.log(stringNumber);
 
+//****************************Opperations ******************* */
+
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// console.log( 2 + 2);
+// console.log( 2 - 2);
+// console.log( 2 * 2);
+// console.log( 2**2);
+
+// console.log( 2 / 2);
+// console.log( 2%3);
+
+
+let str1 = "hello";
+let str2 = " vinod";
+let str3 = str1 + str2;
+console.log(str3);
+
+// problem yahan oe aati h
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log( (3 + 4) * 5 / 8 - 5 % 6);
+
+// console.log(true+1);
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2; // that is tarki quesion
+
+
+// prefix and postfix
+
+// prefix mn value pahle add hojati h or post fix mn value bad mn add hooti h 
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+
+
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
 
 
 
