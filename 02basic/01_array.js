7d34c967922a8d7b34b3082cf742bfe1c8e66e60
Vinod kumar lakhani <10224016+Vinodkumarlakhani1@users.noreply.github.com>
2024-05-29 18:24:14 +0000

    array

---
+++ 02basic/01_array.js
@@ -0,0 +1,42 @@
+// array
+
+let myArray = [2, 5, 5, 6 , 9];
+
+
+const myHeros = ["shaktiman", "naga"]
+//   console.log(myArray[0]);
+//   console.log(myArray.length);
+// myArray.push(6);
+// og(myArray.length);
+// myArray.push(8)
+// myArray.pop()// remove last element 
+// myArray.unshift(6)
+// myArray.shift() // remove start element
+// console.log(myArray.includes(9)); // questiion
+// console.log(myArray.indexOf(2));
+
+// const newArray = myArray.join()
+
+// console.log(myArray);
+// console.log(newArray);
+// console.log(typeof newArray);
+
+// slice , splice 
+
+console.log("A", myArray);
+
+const myn1 =  myArray.slice(1, 3)
+
+console.log(myn1);
+
+console.log("B", myArray);
+
+const myn2 =  myArray.splice(1, 3)
+console.log(myn2);
+console.log("B", myArray);
+
+
+
+
+
+
